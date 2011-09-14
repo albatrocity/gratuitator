6c520b2556f41b0281c3b38f669f83b771365b26
Ross Brown <ross@cremalab.com>
2011-09-14 11:36:21 -0500

    Use .data() instead of .attr('data-attr')

--- jquery.gratuitator.js
+++ jquery.gratuitator.js
@@ -28,7 +28,7 @@
             && $(this).attr(settings.contentAttr) != '' ) {
             var link;
             link = $(this);
-            link.attr('data-gratuitator-title', link.attr('title'))
+            link.data('gratuitator-title', link.attr('title'))
             link.attr('title','');
             popUp(link);
             return drawTriangle(link);
